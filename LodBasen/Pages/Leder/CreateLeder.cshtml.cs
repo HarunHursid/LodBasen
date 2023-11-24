1ee8c73ff76f3b5c3ff49862eb8f61ee6818d657
richardx <richardx10@gmail.com>
2023-11-24 12:35:25 +0100

    Mapper x2

---
+++ LodBasen/Pages/Leder/CreateLeder.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LodBasen.Pages.Leder
+{
+    public class CreateLederModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
