dd7e97507c37c735cfd9b61d9a1bed2d2b7e8f2c
richardx <richardx10@gmail.com>
2023-11-24 12:41:01 +0100

    changes in mapper

---
+++ LodBasen/Pages/Barn/DeleteBarn.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LodBasen.Pages.Barn
+{
+    public class DeleteBarnModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
