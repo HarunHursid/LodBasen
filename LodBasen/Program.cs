e34c2dc2e650f107dd3fd89865fff35597044947
MetteTobiasen <tobiasen_1@hotmail.com>
2023-12-19 19:07:55 +0100

    udkommanteret kode

--- LodBasen/Program.cs
+++ LodBasen/Program.cs
@@ -16,37 +16,5 @@ namespace LodBasen
         {
             webBuilder.UseStartup<Startup>();
         });
-        //    var builder = WebApplication.CreateBuilder(args);
-
-        //    void ConfigureServices(IServiceCollection services)
-        //    {
-        //        //Add services to the container.
-        //        builder.Services.AddRazorPages();
-        //        builder.Services.AddDbContext<lodbasen_dk_db_lodbasenContext>();
-        //        builder.Services.AddTransient<IBarnService, EFBarnService>();
-        //        builder.Services.AddTransient<IGruppeService, EFGruppeService>();
-        //}
-
-        //var app = builder.Build();
-
-        //    // Configure the HTTP request pipeline.
-        //    if (!app.Environment.IsDevelopment())
-        //    {
-        //        app.UseExceptionHandler("/Error");
-        //        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        //        app.UseHsts();
-        //    }
-
-        //    app.UseHttpsRedirection();
-        //    app.UseStaticFiles();
-
-        //    app.UseRouting();
-
-        //    //app.UseAuthorization();
-
-        //    app.MapRazorPages();
-
-        //    app.Run();
-        //}
     }
 }
