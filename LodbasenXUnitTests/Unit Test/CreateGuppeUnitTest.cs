85088f34ccb812cfeb745ae399eab4333dfd13b2
MetteTobiasen <tobiasen_1@hotmail.com>
2024-01-07 22:03:32 +0100

    CreateGruppeUnitTest af OnGet i create gruppe

---
+++ LodbasenXUnitTests/Unit Test/CreateGuppeUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using LodBasen.Models;
+using LodBasen.Pages.Gruppe;
+using LodBasen.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LodbasenXUnitTests.Unit_Test
+{
+    public class CreateGruppeUnitTest
+    {
+
+        [Fact]
+        public void CreateGruppe_Post_ReturnsARedirectAndAddsGruppe_WhenModelStateIsValid()
+        {
+            //Arrange
+
+            var mockRepo = new Mock<IGruppeService>();
+            mockRepo.Setup(repo => repo.AddGruppe(It.IsAny<Gruppe>())).Verifiable();
+
+            var @gruppe = new Gruppe() { GruppeId = 1, GruppeNavn = "Test 1" };
+
+            var createModel = new CreateGruppeModel(mockRepo.Object);
+            
+            createModel.Gruppe = @gruppe;
+
+            //Act 
+            var result = createModel.OnPost();
+
+            //Assert
+            var redirectToActionResult = Assert.IsType<RedirectToPageResult>(result);
+            Assert.Equal("GetGruppe", redirectToActionResult.PageName);
+            mockRepo.Verify((e) => e.AddGruppe(@gruppe), Times.Once);
+        }
+    }
+}
