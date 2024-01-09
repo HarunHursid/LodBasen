e81ca5bdeb9ed864c5c481b90770f11ebb4bebdd
MetteTobiasen <tobiasen_1@hotmail.com>
2024-01-09 12:41:35 +0100

    tilføjet connectionstring til startup

--- LodBasen/Models/lodbasen_dk_db_lodbasenContext.cs
+++ LodBasen/Models/lodbasen_dk_db_lodbasenContext.cs
@@ -33,9 +33,9 @@ public partial class lodbasen_dk_db_lodbasenContext : DbContext
 
     public virtual DbSet<Sælger> Sælgere { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=mssql5.unoeuro.com;Initial Catalog=lodbasen_dk_db_lodbasen;Persist Security Info=True;User ID=lodbasen_dk;Password=ne2GgdbHy3DEmrkAFwtx;TrustServerCertificate=True");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+////#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Data Source=mssql5.unoeuro.com;Initial Catalog=lodbasen_dk_db_lodbasen;Persist Security Info=True;User ID=lodbasen_dk;Password=ne2GgdbHy3DEmrkAFwtx;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
